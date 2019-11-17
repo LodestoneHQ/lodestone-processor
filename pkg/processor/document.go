ef71dac88189e46b56d888c4bbea937891cca965
Jason Kulatunga <jason@thesparktree.com>
2019-11-16 23:04:47 -0800

    adding tika server integration.

--- pkg/processor/document.go
+++ pkg/processor/document.go
@@ -1,8 +1,63 @@
 package processor
 
-func DocumentProcessor(body []byte, storageUrl string) error {
+import (
+	"context"
+	"encoding/json"
+	"github.com/analogj/lodestone-processor/pkg/model"
+	"io/ioutil"
+	"log"
+	"os"
+)
+import "github.com/google/go-tika/tika"
+
+func DocumentProcessor(body []byte, storageEndpoint string) error {
+
+	//make a temporary directory for subsequent processing (original file download, and thumb generation)
+	dir, err := ioutil.TempDir("", "doc")
+	if err != nil {
+		return err
+	}
+	defer os.RemoveAll(dir) // clean up
+
+	var event model.S3Event
+	err = json.Unmarshal(body, &event)
+	if err != nil {
+		return err
+	}
+
+	docBucketName, docBucketPath, err := generateStoragePath(event)
+	if err != nil {
+		return err
+	}
+
+	filePath, err := retrieveDocument(storageEndpoint, docBucketName, docBucketPath, dir)
+	if err != nil {
+		return err
+	}
 
 	//TODO pass document to TIKA
+	err = parseDocument(filePath)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func parseDocument(localFilePath string) error {
+
+	f, err := os.Open(localFilePath)
+	if err != nil {
+		return err
+	}
+
+	client := tika.NewClient(nil, "http://tika:9998")
+	body, err := client.Parse(context.Background(), f)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("%s", body)
 
 	return nil
 }
